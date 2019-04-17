d2e3a338d56bae289d5c481442901c4613e0f43e
Lars Grahmann <lars@kloud.software>
2019-04-17 21:10:29 +0200

    update entities

---
+++ src/main/java/software/kloud/KMSPluginSDK/AbsController.java
@@ -0,0 +1,11 @@
+package software.kloud.KMSPluginSDK;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+
+import java.security.Principal;
+
+public class AbsController {
+    public String getUsernameFromPrincipal(Principal principal) {
+        return (String) ((UsernamePasswordAuthenticationToken) principal).getPrincipal();
+    }
+}
