17372669679043d00b5932951b5e4677285e7e58
fred <fredlahde@gmail.com>
2019-04-25 10:48:45 +0200

    add convience methods for roleService

--- src/main/java/software/kloud/KMSPluginSDK/RoleService.java
+++ src/main/java/software/kloud/KMSPluginSDK/RoleService.java
@@ -3,5 +3,7 @@ package software.kloud.KMSPluginSDK;
 import software.kloud.kms.entities.RoleJpaRecord;
 
 public interface RoleService {
+    RoleJpaRecord getAdminRole();
+    RoleJpaRecord getUserRole();
     RoleJpaRecord getRoleFor(String group);
 }
